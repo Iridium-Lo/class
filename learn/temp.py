c087876e24df3bf33eac00d692178e781bd779ea
iridium <iridium@yourmum.com>
2021-06-14 04:13:28 +0100

    x

--- learn/temp.py
+++ learn/temp.py
@@ -2,7 +2,7 @@ from typing import Dict
 
 
 class ConvertToKelvin:
-    def __init__(self, substance: str, temp: Dict[str, float]):
+    def __init__(self, substance: str, temp: Dict[str, int]):
         self.substance = substance
         self.temp = temp
 
@@ -10,28 +10,28 @@ class ConvertToKelvin:
         return celsius + 273
 
     def __str__(self):
-        x = self.substance
-        y = self.to_kelvin(self.temp["mp"])
-        z = self.to_kelvin(self.temp["bp"])
+        sub = self.substance
+        mp = self.to_kelvin(self.temp["mp"])
+        bp = self.to_kelvin(self.temp["bp"])
 
-        return f'{x}\n mp: {y}K\n bp: {z}K\n'
+        return f'{sub}:\n mp: {mp}K\n bp: {bp}K\n'
 
 
 mp_bp_celsius = (
-  ConvertToKelvin(
-      'water',
-      {
-         'bp': 0,
-         'mp': 100
-      }
-  ),
-  ConvertToKelvin(
-      'imaginary',
-      {
-        'bp': 30,
-        'mp': 120
-      }
-  ),
+    ConvertToKelvin(
+        'water',
+        {
+            'bp': 0,
+            'mp': 100
+        }
+    ),
+    ConvertToKelvin(
+        'imaginary',
+        {
+           'bp': 30,
+           'mp': 120
+        }
+    ),
 )
 
 print('\n'.join(str(i) for i in mp_bp_celsius))
