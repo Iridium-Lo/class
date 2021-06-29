2f870ea81c6866e2e8737abf1bc8c07d08b8cff8
iridium <iridium@yourmum.com>
2021-06-30 00:11:22 +0100

    x

---
+++ product/productNoTax.py
@@ -0,0 +1,64 @@
+from _decimal import Decimal
+from dataclasses import dataclass
+from typing import Collection
+
+
+@dataclass
+class Product:
+    name: str
+    price: int
+
+    @property
+    def round(self) -> Decimal:
+        return round(self.price, 2)
+
+    @property
+    def format_row(self) -> str:
+        return (
+            f'{self.name:10} '
+            f'{self.round} '
+        )
+
+
+@dataclass
+class Order:
+    products: Collection[Product]
+
+    @property
+    def total(self) -> Decimal:
+        return sum(i.round for i in self.products)
+
+    @property
+    def print(self) -> None:
+        print('\n'.join(p.format_row for p in self.products))
+        print(f'\ntotal {self.total:10}')
+
+
+def main():
+    cosmetics = Order(
+        products=[
+            Product(
+                name='saly', price=Decimal(6.99)
+            ),
+            Product(
+                name='kojic', price=Decimal(7.99)
+            ),
+            Product(
+                name='jojoba', price=Decimal(7.45)
+            ),
+            Product(
+                name='gylcol', price=Decimal(9.99)
+            ),
+            Product(
+                name='jojoba', price=Decimal(7.45)
+            ),
+            Product(
+                name='bandage', price=Decimal(6.95)
+            )
+        ],
+    )
+    cosmetics.print
+
+
+if __name__ == '__main__':
+    main()
---
+++ tracker/track.py
@@ -0,0 +1,75 @@
+import calendar as c
+from datetime import time as t
+from decimal import Decimal
+from dataclasses import dataclass
+from typing import Dict, Iterable, Collection
+
+UNIT = 'u'
+SUBSTANCE = 's'
+
+
+@dataclass
+class DailyAverageDose:
+    day: int
+    time_dose: Dict[float, Decimal]
+
+    @property
+    def times(self) -> list[float]:
+        return list(self.time_dose.keys())
+
+    @property
+    def doses(self) -> list[Decimal]:
+        return list(self.time_dose.values())
+
+    @property
+    def daily_dose(self) -> Decimal:
+        return sum(i for i in self.doses)
+
+    def diff(self, times_doses) -> Iterable:
+        return (abs(i - i+1) for i in times_doses)
+
+    def mean(self, times_doses) -> Decimal:
+        return sum(i for i in self.diff(times_doses)) / len(times_doses)
+
+    def __str__(self):
+        return {
+            f'{self.day}: {self.daily_dose}'
+            f'Mtime: {self.mean(self.times)}'
+            f'Mdose: {self.mean(self.doses)}'
+        }
+
+
+@dataclass
+class DoseWeekday:
+    day_time_dose: Collection[DailyAverageDose]
+
+    def dose_total(self) -> float:
+        return sum(i for i in self.doses.values())
+
+    def weekday_name(self) -> str:
+        return (i for i in self.days)
+
+    def __str__(self):
+        return f'{self.weekday_name}: {self.dose_total} {UNIT}'
+
+
+def main():
+   days = DoseWeekday(
+       day_time_dose=(
+           DailyAverageDose(
+               day=c.MONDAY,
+               time_dose={
+                 t(12): 1,
+                 t(13): 1,
+               },
+           ),
+           DailyAverageDose(
+               day=c.TUESDAY,
+               time_dose={
+                 t(12): 2,
+                 t(14): 2,
+               }
+           ),
+       )
+   )
+   days.echo
