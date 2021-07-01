4897fcbdf75ff522e85ca75ee57fee1485253a47
Iridium-Lo <80277972+Iridium-Lo@users.noreply.github.com>
2021-07-01 02:59:54 +0100

    Update track.py

--- tracker/track.py
+++ tracker/track.py
@@ -17,7 +17,7 @@ class DailyDoseMean:
     def daily_dose(self) -> Decimal:
         return sum(i for i in self.doses)
 
-def mean_lst(self, num, key_val) -> Decimal:
+    def mean_lst(self, num, key_val) -> Decimal:
         return self.mean(self.lst(key_val, num))
 
     def mean(self, lst) -> Decimal:
@@ -71,6 +71,8 @@ class WeeklyDose:
     def echo(self):
         print('\n'.join(i.str for i in self.day_dose_mean))
         print(f'Weekly dose -> {SUBSTANCE}: {self.weekly_dose} {UNIT}'
+        print(f'Weekly dose -> mean: {self.weekly_mean(2, values)}')
+        print(f'Weekly time -> mean: {self.weekly_mean(4, keys)}')
 
 
 def main():
