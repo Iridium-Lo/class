4c2d5a6e999259e38d68a7a2b803f7f57984eb11
Iridium-Lo <80277972+Iridium-Lo@users.noreply.github.com>
2021-07-01 02:45:58 +0100

    Update track.py

--- tracker/track.py
+++ tracker/track.py
@@ -58,8 +58,9 @@ class WeeklyDose:
    
     def weekly_mean(self, num, key_val) -> Decimal:
         return sum(
-            i.mean_lst(num, key_val) for i in self.day_dose_mean
-        ) / len(i.day for i in self.day_dose_mean)
+            i.mean_lst(num, key_val) 
+            for i in self.day_dose_mean
+        ) / 7
     
     @property
     def echo(self):
